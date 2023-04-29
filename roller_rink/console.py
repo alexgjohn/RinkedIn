81a9b8ccef1954726f919a10bae72ce943c8b66c
Alex <alexgjohn@outlook.com>
2023-04-29 13:22:39 +0100

    repos almost done, feels great

--- roller_rink/console.py
+++ roller_rink/console.py
@@ -4,9 +4,9 @@ from models.lesson import Lesson
 from models.level import Level
 import repositories.skater_repository as skater_repo
 import repositories.lesson_repository as lesson_repo
-# import repositories.level_repository as level_repo
+import repositories.level_repository as level_repo
 
-# level_repo.delete_all()
+level_repo.delete_all()
 skater_repo.delete_all()
 lesson_repo.delete_all()
 
@@ -19,3 +19,9 @@ lesson1 = Lesson("Speed Skating", "Monday", 10, False)
 lesson_repo.save(lesson1)
 lesson2 = Lesson("Artistic Skating", "Wednesday", 12, False)
 lesson_repo.save(lesson2)
+
+level1 = Level(skater1, lesson1, "Beginner")
+level_repo.save(level1)
+level2 = Level(skater2, lesson1, "Intermediate")
+level_repo.save(level2)
+
--- roller_rink/repositories/level_repository.py
+++ roller_rink/repositories/level_repository.py
@@ -2,3 +2,60 @@ from db.run_sql import run_sql
 from models.skater import Skater
 from models.lesson import Lesson
 from models.level import Level
+import repositories.skater_repository as skater_repo
+import repositories.lesson_repository as lesson_repo
+
+
+def save(level):
+    sql = "INSERT INTO levels (skater_id, lesson_id, level_reached) VALUES (%s, %s, %s) RETURNING id"
+    values = [level.skater.id, level.lesson.id, level.level_reached]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    level.id = id
+    return level
+
+
+def select_all():
+    levels = []
+    sql = "SELECT * FROM levels"
+    results = run_sql(sql)
+    for row in results:
+        skater = skater_repo.select(row['skater_id'])
+        lesson = lesson_repo.select(row['lesson_id'])
+        level = Level(skater, lesson, row['level_reached'], row['id'])
+        levels.append(level)
+    return levels
+
+
+def select(id):
+    level = None
+    sql = "SELECT * FROM levels WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    if results:
+        result = results[0]
+        skater = skater_repo.select(result['skater_id'])
+        lesson = lesson_repo.select(result['lesson_id'])
+        level = Level(skater, lesson, result['level_reached'], result['id'])
+    return level
+
+
+def delete_all():
+    sql = "DELETE from levels"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE from levels WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+# bit confused about this one, so I might save it until Monday. Can't currently see the use for it either.
+# def update(level):
+    # sql = "UPDATE levels SET (skater_id, lesson_id, level_reached) = (%s, %s, %s) WHERE id = %s"
+    # values = 
+
+# would also be useful to have:
+    # get levels for lesson
+    # get levels for skater
