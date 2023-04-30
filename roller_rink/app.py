d3bb1e44adbf91eac24acc1e424d5d276b182792
Alex <alexgjohn@outlook.com>
2023-04-30 12:43:56 +0100

    started adding frontend files

--- roller_rink/app.py
+++ roller_rink/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 # from controllers.skaters_controller import skaters_blueprint
-# from controllers.lessons_controller import lessons_blueprint
+from controllers.lessons_controller import lessons_blueprint
 # from controllers.levels_controller import levels_blueprint
 app = Flask(__name__)
 
 # app.register_blueprint(skaters_blueprint)
-# app.register_blueprint(lessons_blueprint)
+app.register_blueprint(lessons_blueprint)
 # app.register_blueprint(levels_bleuprint)
 
 @app.route('/')
 def home():
-    return render_template('index.jinja')
+    return render_template('home.jinja')
+
+@app.route('/about')
+def about():
+    return render_template('about.jinja')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ roller_rink/controllers/lessons_controller.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.lesson import Lesson
+import repositories.lesson_repository as lesson_repo
+import repositories.skater_repository as skater_repo
+import repositories.level_repository as level_repo
+
+lessons_blueprint = Blueprint("lessons", __name__)
+
+# route for lessons - get 
+
+# route for lesson - get 
+
+# route for new lesson - get 
+
+# route for create lesson - post
+    
+# route for change lesson - post 
+
+# route for update lesson - post
+    
+# route for delete lesson - post - NOT MVP
