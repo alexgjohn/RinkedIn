67bf01f5ec08eadbce4ea707a6bd2aefa3c8ce31
Alex <alexgjohn@outlook.com>
2023-05-01 12:00:18 +0100

    added create skater form

--- roller_rink/controllers/skaters_controller.py
+++ roller_rink/controllers/skaters_controller.py
@@ -19,3 +19,27 @@ def show(id):
     skater = skater_repo.select(id)
     title = skater.full_name
     return render_template('skaters/show.jinja', skater = skater, title = title)
+
+@skaters_blueprint.route('/skaters/new', methods = ['GET'])
+def new_skater():
+    return render_template('skaters/new.jinja', title = "Register a New Skater")
+
+
+@skaters_blueprint.route('/skaters', methods = ['POST'])
+def add_skater():
+    full_name = request.form['full_name']
+    premium_member = request.form['premium_member']
+    skater = Skater(full_name, premium_member)
+    skater_repo.save(skater)
+    return redirect('/skaters')
+
+
+# @lessons_blueprint.route('/lessons', methods = ['POST'])
+# def add_lesson():
+#     name = request.form['name']
+#     day = request.form['day']
+#     capacity = int(request.form['capacity'])
+#     premium = request.form['premium']
+#     lesson = Lesson(name, day, capacity, premium)
+#     lesson_repo.save(lesson)
+#     return redirect('/lessons')
