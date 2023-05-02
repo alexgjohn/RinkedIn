69490e14e3c37e77781c17ecb2ba7b49b2592547
Alex <alexgjohn@outlook.com>
2023-05-02 16:11:04 +0100

    very nearly cracked these two extensions

--- roller_rink/controllers/lessons_controller.py
+++ roller_rink/controllers/lessons_controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.lesson import Lesson
+from models.skater import Skater
+from models.level import Level
 import repositories.lesson_repository as lesson_repo
 import repositories.skater_repository as skater_repo
 import repositories.level_repository as level_repo
@@ -19,7 +21,8 @@ def show(id):
     lesson = lesson_repo.select(id)
     skaters_in_lesson = skater_repo.get_skaters_in_lesson(lesson)
     skater_count = len(skaters_in_lesson)
-    return render_template('lessons/show.jinja', lesson = lesson, skaters_in_lesson = skaters_in_lesson, title = lesson.name, skater_count = skater_count)
+    spaces_in_lesson = lesson.capacity - skater_count
+    return render_template('lessons/show.jinja', lesson = lesson, skaters_in_lesson = skaters_in_lesson, title = lesson.name, skater_count = skater_count, spaces_in_lesson = spaces_in_lesson)
 
 # route for new lesson - get 
 @lessons_blueprint.route('/lessons/new', methods = ['GET'])
@@ -66,7 +69,7 @@ def update_lesson(id):
 @lessons_blueprint.route('/lessons/<id>/delete', methods=['POST'])
 def delete_lesson(id):
     lesson_repo.delete(id)
-    return render_template('home.jinja')
+    return redirect('/lessons')
 
 
 @lessons_blueprint.route('/lessons/<id>/book', methods=['GET'])
@@ -75,4 +78,21 @@ def book_skater_to_lesson(id):
     skaters = skater_repo.select_all()
     premium_skaters = skater_repo.get_premium_skaters()
     return render_template('lessons/book.jinja', title = "Add a skater to this lesson!", lesson = lesson, skaters = skaters, premium_skaters = premium_skaters)
+
+
+@lessons_blueprint.route('/lessons/<id>/book', methods = ['POST'])
+def add_level_for_lesson(id):
+    lesson_id = request.form['lesson_id']
+    skater_id = request.form['skater_id']
+    skater = skater_repo.select(skater_id)
+    lesson = lesson_repo.select(lesson_id)
+    level_reached = request.form['level']
+    level = Level(skater, lesson, level_reached)
+    result = level_repo.check_for_duplicate(level)
+    if result == True:
+        return redirect('/error')
+    else:
+        level_repo.save(level)
+        return redirect('/lessons')
+
 
